a1cc81cf3d0f9c7e11ca0ab98305946ad0d6d0ee
Shubham Kumar <53478578+shubham1532@users.noreply.github.com>
2020-10-09 02:45:05 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function myFunction() {
+  var number = parseInt(document.getElementById("input").value);
+  console.log(number);
+  var resultA = number.toString(2);
+  var resultB = number.toString(8);
+  var resultC = number.toString(16);
+
+  document.getElementById("answerA").value = resultA;
+  document.getElementById("answerB").value = resultB;
+  document.getElementById("answerC").value = resultC;
+
+}
+
+document.getElementById("calculate").addEventListener("click", myFunction);
